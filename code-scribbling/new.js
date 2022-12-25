03cd6c4454b49234f919ab7dcbd63ac405696083
Tirtharaj <69608898+Tirtharaj-888@users.noreply.github.com>
2022-12-25 18:39:55 +0530

    Add files via upload
    First commit

---
+++ code-scribbling/new.js
@@ -0,0 +1,28 @@
+var num = 15;
+switch (num)
+{
+    case 8:
+        for(i=1;i<=10;i++)
+        {
+            console.log(`8 * ${i} = ${8*i}`);
+        }
+    break;
+    case 9:
+        for(i=1;i<=10;i++)
+        {
+            console.log(`9 * ${i} = ${9*i}`);
+        }
+    break;
+    case 12:
+        for(i=1;i<=10;i++)
+        {
+            console.log(`12 * ${i} = ${12*i}`);
+        }
+    break;
+    case 15:
+        for(i=1;i<=10;i++)
+        {
+            console.log(`15 * ${i} = ${15*i}`);
+        }
+    break;
+}
