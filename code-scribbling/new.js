cb443c3940d97750f3b1c1cc4555e1dc215a9f7c
TRM97 <ananda.utsab@gmail.com>
2022-12-30 21:22:55 +0530

    Created folder practice-book

--- code-scribbling/new.js
+++ code-scribbling/new.js
@@ -1,30 +1,50 @@
 // Multiplication tables solution using swith-case-break
 
-var num = 15;
-switch (num)
-{
-    case 8:
-        for(i=1;i<=10;i++)
-        {
-            console.log(`8 * ${i} = ${8*i}`);
-        }
-    break;
-    case 9:
-        for(i=1;i<=10;i++)
-        {
-            console.log(`9 * ${i} = ${9*i}`);
-        }
-    break;
-    case 12:
-        for(i=1;i<=10;i++)
-        {
-            console.log(`12 * ${i} = ${12*i}`);
-        }
-    break;
-    case 15:
-        for(i=1;i<=10;i++)
-        {
-            console.log(`15 * ${i} = ${15*i}`);
-        }
-    break;
+// var num = 15;
+// switch (num)
+// {
+//     case 8:
+//         for(i=1;i<=10;i++)
+//         {
+//             console.log(`8 * ${i} = ${8*i}`);
+//         }
+//     break;
+//     case 9:
+//         for(i=1;i<=10;i++)
+//         {
+//             console.log(`9 * ${i} = ${9*i}`);
+//         }
+//     break;
+//     case 12:
+//         for(i=1;i<=10;i++)
+//         {
+//             console.log(`12 * ${i} = ${12*i}`);
+//         }
+//     break;
+//     case 15:
+//         for(i=1;i<=10;i++)
+//         {
+//             console.log(`15 * ${i} = ${15*i}`);
+//         }
+//     break;
+// }
+
+// if-else problems
+
+// let num2 = 15 
+// if(num2==1){
+//     console.log(`${num2} means light is ON`);
+// }
+// else if(num2==0){
+//     console.log(`${num2} means light is OFF`);
+// }
+// else{
+//     console.log(`${num2} is an invalid input. Only 0 & 1 are valid inputs for the light`);
+// }
+
+function sq(number){
+    let numSq = number*number;
+    debugger;
+    return numSq;
 }
+console.log(sq(12));
